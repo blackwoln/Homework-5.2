16b7072c47d2bdd0859d18071ca84ce7d8f924b2
vkray <vitalikkrainyuk@yandex.ru>
2023-04-19 13:01:26 +0300

    master. Циклы. Урок 2

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();// Задача 1
+        task2();// Задача 2
+        task3();// Задача 3
+        task4();// Задача 4
+        task5();// Задача 5
+        task6();// Задача 6
+        task7();// Задача 7
+        task8();// Задача 8
+    }
+
+    public static void task1 () {
+
+    }
+
+    public static void task2 () {
+
+    }
+
+    public static void task3 () {
+
+    }
+
+    public static void task4 () {
+
+    }
+
+    public static void task5 () {
+
+    }
+
+    public static void task6 () {
+
+    }
+
+    public static void task7 () {
+
+    }
+
+    public static void task8 () {
+    }
+
+}
